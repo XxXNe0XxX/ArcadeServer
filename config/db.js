3b1a6f01752e6b5cde82d8c7cb380ded450a8532
XxXNe0XxX <jm82467913@gmail.com>
2024-07-02 13:00:59 -0500

    updated env variables

--- config/db.js
+++ config/db.js
@@ -3,11 +3,14 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const createDatabase = async () => {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  });
+  const connection = await mysql.createConnection(
+    // {
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // }
+    process.env.MYSQL_URL
+  );
 
   await connection.query(
     `CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`
